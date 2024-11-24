fff473eccc6e69186abacdb24720ba90a8b9e09d
Christopher Marcellino Kristiono Putra <excelcmkp638@gmail.com>
2024-11-24 10:57:33 +0700

    Add files via upload

---
+++ Vid-php-29/restoran/LATIHAN/function.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $host = "localhost:8111";
+    $user = "root";
+    $password = "";
+    $database = "dbrestoran";
+
+    $koneksi = mysqli_connect($host,$user,$password,$database);
+
+?>
